add979085588109ba299aafc30493b9ef4b225a4
Nathan <hui.n1998@gmail.com>
2022-07-23 18:32:10 +0100

    flyout component and styling

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,14 +1,33 @@
-import { ReactNode } from "react";
-import Navbar from "../Navbar/Navbar";
+import { ReactNode, useState } from "react";
+import Navbar, { Flyout } from "../Navbar/Navbar";
 import s from "./Layout.module.scss";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const [flyoutActive, setFlyoutActive] = useState(false);
+
+  const openFlyout = () => {
+    document.body.style.overflowY = "hidden";
+    setFlyoutActive(true);
+  };
+
+  const closeFlyout = () => {
+    document.body.style.overflowY = "auto";
+    setFlyoutActive(false);
+  };
+
   return (
     <div className={s.layout}>
+      <Flyout flyoutActive={flyoutActive} closeFlyout={closeFlyout} />
+      <button
+        className={`${s.overlay} ${flyoutActive && s.active}`}
+        onClick={closeFlyout}
+      />
       <header className={s.header}>
-        <Navbar />
+        <Navbar openFlyout={openFlyout} />
       </header>
-      <div className={s.content}>{children}</div>
+      <main className={`${s.content} ${flyoutActive && s.active}`}>
+        {children}
+      </main>
     </div>
   );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,22 +1,55 @@
 import Link from "next/link";
 import { links } from "./data";
 import s from "./Navbar.module.scss";
+import { IoCloseOutline } from "react-icons/io5";
+import { Dispatch, SetStateAction } from "react";
 
-export default function Navbar() {
+interface NavbarProps {
+  openFlyout: () => void;
+}
+
+export default function Navbar({ openFlyout }: NavbarProps) {
   return (
-    <nav className={s.navbar}>
-      <ol className={s.links}>
-        {links.map(({ id, href, text }) => (
-          <li className={s.link} key={id}>
-            <Link href={href}>
-              <a>
-                <span>0{id}.</span>
-                {text}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ol>
-    </nav>
+    <>
+      <nav className={s.navbar}>
+        <Links />
+        <button className={s.hamburger} onClick={openFlyout}>
+          <div className={s.line} />
+          <div className={s.line} />
+          <div className={s.line} />
+        </button>
+      </nav>
+    </>
   );
 }
+
+interface FlyoutProps {
+  flyoutActive: boolean;
+  closeFlyout: () => void;
+}
+
+export const Flyout = ({ flyoutActive, closeFlyout }: FlyoutProps) => {
+  return (
+    <aside className={`${s.flyout} ${flyoutActive && s.active}`}>
+      <button className={s.close} onClick={closeFlyout}>
+        <IoCloseOutline />
+      </button>
+      <Links />
+    </aside>
+  );
+};
+
+const Links = () => (
+  <ol className={s.links}>
+    {links.map(({ id, href, text }) => (
+      <li className={s.link} key={id}>
+        <Link href={href}>
+          <a>
+            <span>0{id}.</span>
+            {text}
+          </a>
+        </Link>
+      </li>
+    ))}
+  </ol>
+);
