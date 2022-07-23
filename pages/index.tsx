635c41b036cfad922ed95122bfbd69b5b15ceb42
Nathan <hui.n1998@gmail.com>
2022-07-23 13:16:57 +0100

    add navbar to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Navbar from "../components/Navbar/Navbar";
+import s from "../styles/pages/homepage.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={s.homepage}>
       <Head>
         <title>Nathan H. | Portfolio</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
     </div>
   );
 };
