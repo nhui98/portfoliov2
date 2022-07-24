1b8ab1a9b3879ec55c5c5616dc0400f62dd6b68b
Nathan <hui.n1998@gmail.com>
2022-07-24 23:32:51 +0100

    refactor project, move data inside component

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -2,56 +2,73 @@ import SectionHeading from "../SectionHeading/SectionHeading";
 import s from "./Projects.module.scss";
 import { data } from "./data";
 import Link from "next/link";
+import { IconType } from "react-icons";
 
 export default function Projects() {
   const { counter, title, projects } = data;
 
   return (
-    <div className={s.projects}>
+    <section className={s.projects} id="projects">
       <SectionHeading counter={counter} title={title} />
-      {projects.map(
-        ({
-          id,
-          productImage,
-          projectDescription,
-          projectStack,
-          projectTitle,
-          links,
-        }) => (
-          <div className={`${s.grid} ${id % 2 === 0 && s.alt}`} key={id}>
-            <div className={s.image}>
-              <img src={productImage} alt={projectTitle} />
-            </div>
-            <div className={s.details}>
-              <h1 className={s.label}>Featured Project</h1>
-              <h2 className={s.name}>{projectTitle}</h2>
-              <div className={s.description}>
-                <p>{projectDescription}</p>
-                <ul className={s.stack}>
-                  {projectStack.map((item) => (
-                    <li className={s.item} key={item}>
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={s.links}>
-                {links.map(({ icon: Icon, link }, i) => (
-                  <Link href={link} passHref key={i}>
-                    <a
-                      className={s.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Icon />
-                    </a>
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        )
-      )}
-    </div>
+      {projects.map((project) => (
+        <ProjectItem key={project.id} {...project} />
+      ))}
+    </section>
   );
 }
+
+interface ProjectItemProps {
+  id: number;
+  label: string;
+  productImage: string;
+  projectTitle: string;
+  projectDescription: string;
+  projectStack: string[];
+  links: {
+    icon: IconType;
+    link: string;
+  }[];
+}
+
+const ProjectItem = ({
+  id,
+  label,
+  productImage,
+  projectTitle,
+  projectDescription,
+  projectStack,
+  links,
+}: ProjectItemProps) => {
+  const altClass = id % 2 === 0 && s.alt;
+
+  return (
+    <div className={s.project}>
+      <div className={`${s.image} ${altClass} golden-overlay`}>
+        <img src={productImage} alt={projectTitle} />
+      </div>
+      <div className={`${s.details} ${altClass}`}>
+        <h1 className={s.label}>{label}</h1>
+        <h2 className={s.name}>{projectTitle}</h2>
+        <div className={s.description}>
+          <p>{projectDescription}</p>
+          <ul className={`${s.stack}  ${altClass}`}>
+            {projectStack.map((item) => (
+              <li className={s.item} key={item}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`${s.links} ${altClass}`}>
+          {links.map(({ icon: Icon, link }, i) => (
+            <Link href={link} passHref key={i}>
+              <a className={s.link} target="_blank" rel="noopener noreferrer">
+                <Icon />
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
