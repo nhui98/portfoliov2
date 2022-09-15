a44d8c80f885ed78a2ffc35212a7bf28a4cb0759
Nathan <hui.n1998@gmail.com>
2022-09-15 19:09:05 +0100

    syntax refactor

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 import s from "./Footer.module.scss";
 
-export default function Footer() {
-  return (
-    <footer className={s.footer}>
-      <div className={s.socials}>
-        {SOCIAL_LINKS.map(({ href, icon: Icon, id }) => (
-          <Link href={href} passHref key={id}>
-            <a className={s.social} target="_blank">
-              <Icon />
-            </a>
-          </Link>
-        ))}
-      </div>
-    </footer>
-  );
-}
+const Footer = () => (
+  <footer className={s.footer}>
+    <div className={s.socials}>
+      {SOCIAL_LINKS.map(({ href, icon: Icon, id }) => (
+        <Link href={href} passHref key={id}>
+          <a className={s.social} target="_blank">
+            <Icon />
+          </a>
+        </Link>
+      ))}
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/components/Common/Sidebar/Sidebar.tsx
+++ src/components/Common/Sidebar/Sidebar.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 import FadeSection from "../../../utils/FadeSection";
 import s from "./Sidebar.module.scss";
 
-export default function Sidebar() {
-  return (
-    <FadeSection>
-      <div className={s.sidebar}>
-        <div className={s.line} />
-        <div className={s.socials}>
-          {SOCIAL_LINKS.map(({ href, icon: Icon, id }) => (
-            <Link href={href} passHref key={id}>
-              <a className={s.social} target="_blank" rel="noopener noreferrer">
-                <Icon />
-              </a>
-            </Link>
-          ))}
-        </div>
-        <div className={s.line} />
+const Sidebar = () => (
+  <FadeSection>
+    <div className={s.sidebar}>
+      <div className={s.line} />
+      <div className={s.socials}>
+        {SOCIAL_LINKS.map(({ href, icon: Icon, id }) => (
+          <Link href={href} passHref key={id}>
+            <a className={s.social} target="_blank" rel="noopener noreferrer">
+              <Icon />
+            </a>
+          </Link>
+        ))}
       </div>
-    </FadeSection>
-  );
-}
+      <div className={s.line} />
+    </div>
+  </FadeSection>
+);
+
+export default Sidebar;
