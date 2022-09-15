fd042471c03cabfbd511585f2d2197e5f6b8ff93
Nathan <hui.n1998@gmail.com>
2022-09-15 15:43:44 +0100

    refactor hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,19 +1,25 @@
-import s from "./Hero.module.scss";
 import Link from "next/link";
-import { data } from "./Hero.data";
+
+import { FadeSection } from "../../utils/FadeSection";
+import s from "./Hero.module.scss";
 
 export default function Hero() {
-  const { intro, name, subtitle, about, resumeLink, resumeText } = data;
-
   return (
-    <section className={s.hero}>
-      <h1 className={s.intro}>{intro}</h1>
-      <h2 className={s.name}>{name}</h2>
-      <h3 className={s.title}>{subtitle}</h3>
-      <p className={s.about}>{about}</p>
-      <Link href={resumeLink}>
-        <a className={s.resume}>{resumeText}</a>
-      </Link>
-    </section>
+    <FadeSection direction="up">
+      <section className={s.hero}>
+        <h1 className={s.intro}>Hi, my name is</h1>
+        <h2 className={s.name}>Nathan Hui.</h2>
+        <h3 className={s.title}>I&apos;m a Web Developer.</h3>
+        <p className={s.about}>
+          I focus on front-end technologies and frameworks such as JavaScript
+          and React to build incredible digital experiences for the web.
+        </p>
+        <Link href="/documents/resume.pdf">
+          <a target="_blank" className={s.resume}>
+            Resume
+          </a>
+        </Link>
+      </section>
+    </FadeSection>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Experience from "../components/Experience/Experience";
@@ -7,7 +8,6 @@ import Hero from "../components/Hero/Hero";
 import OtherProjects from "../components/OtherProjects/OtherProjects";
 import Projects from "../components/Projects/Projects";
 import s from "../styles/pages/index.module.scss";
-import { FadeSection } from "../utils/FadeSection";
 
 const Home: NextPage = () => (
   <div className={s.home}>
@@ -17,9 +17,7 @@ const Home: NextPage = () => (
       <link rel="icon" href="icons/favicon.ico" />
     </Head>
 
-    <FadeSection direction="up">
-      <Hero />
-    </FadeSection>
+    <Hero />
 
     <About />
 
