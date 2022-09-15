4d5bd89069cc28163e5163a384e239f6bd3e1672
Nathan <hui.n1998@gmail.com>
2022-09-15 18:24:58 +0100

    refactor contact me component

--- components/Contact/Contact.data.ts
+++
@@ -1,9 +0,0 @@
-export const data = {
-  counter: "04.",
-  title: "Contact me",
-  subtitle: "Lets get in touch",
-  description:
-    "Feel free to send me an email if you have any questions or just want to talk!",
-  btnText: "Message me",
-  btnLink: "mailto:hui.nathan9@gmail.com",
-};
---
+++ src/components/Home/Contact/Contact.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import FadeSection from "../../../utils/FadeSection";
+import s from "./Contact.module.scss";
+
+const Contact = () => (
+  <section className={s.contact} id="contact">
+    <FadeSection direction="up" delay=".25s">
+      <h1 className={s.title}>
+        <span className={s.counter}>04.</span>
+        Contact me
+      </h1>
+    </FadeSection>
+    <FadeSection direction="up" delay=".4s">
+      <h2 className={s.subtitle}>Lets get in touch</h2>
+    </FadeSection>
+    <FadeSection direction="up" delay=".55s">
+      <p className={s.description}>
+        Feel free to send me an email if you have any questions or just want to
+        talk!
+      </p>
+    </FadeSection>
+    <FadeSection direction="up" delay=".7s">
+      <div className={s.link}>
+        <Link href="mailto:hui.nathan9@gmail.com">
+          <a>Message me</a>
+        </Link>
+      </div>
+    </FadeSection>
+  </section>
+);
+
+export default Contact;
